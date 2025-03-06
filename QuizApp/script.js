7dc4be2b6cd6e2b11ab2af03c3c73ce63f44f1bb
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-03-06 23:56:17 +0530

    upadate quiz

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -9,7 +9,7 @@ const questions = [
         ]
     },
     {
-        question:"Which is largest animal in the world?",
+        question:"Most Beautifull country in the world?",
         answers:[
             {text:"Shark",correct:false},
             {text:"Blue whale",correct:false},
@@ -38,7 +38,7 @@ const questions = [
 ];
 
 const qustionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-button");
+const answerButtons = document.getElementById("answer-button");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -63,7 +63,60 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer)
     });
 }
+
+function resetState(){
+     nextButton.style.display = "none";
+     while(answerButton.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+     }
+}
+
+function selectAnswer(e){
+    const selectBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+         selectedBtn.classList.add("correct");
+    }else{
+         selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct == "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
 
 startQuiz();
